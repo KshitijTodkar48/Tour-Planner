23b83eeac01f2bb357549b1d966b2f9e66018c1e
Kshitij Todkar <kshitijtodkar4826@gmail.com>
2023-04-28 00:34:20 +0530

    added Locations.jsx

--- src/App.js
+++ src/App.js
@@ -1,37 +1,56 @@
-import React from "react";
-import "./App.css" ;
+import React, { useState } from "react";
+import "./App.css";
+import Location from "./components/Location";
 
 function App() {
+  
+ const [category,setCategory] = useState("") ;
+
+  const clickHandler = () => {
+    const selectElement1 = document.getElementById("options1");
+    const selectElement2 = document.getElementById("options2");
+    const selectedOption1 = selectElement1.value;
+    const selectedOption2 = selectElement2.value;
+    if(selectedOption1 && selectedOption2)
+      {
+        setCategory(selectedOption2) ;
+      }
+    else
+      {
+         alert("Please select both the fields.") ;
+      }
+        
+  };
+
   return (
     <div className="App">
       <div className="container">
-
         <div className="input">
-          <label for="options">City to tour : </label>
-          <select id="options" name="options">
+          <label for="options1">City to tour : </label>
+          <select id="options1" name="options1">
             <option value="" disabled selected hidden>
               Select a city
             </option>
-            <option value="option1">Pune</option>
+            <option value="Pune">Pune</option>
           </select>
         </div>
 
         <div className="input">
-          <label for="options">Area of interest for tourist places : </label>
-          <select id="options" name="options">
+          <label for="options2">Area of interest for tourist places : </label>
+          <select id="options2" name="options2">
             <option value="" disabled selected hidden>
               Select an option
             </option>
-            <option value="option1">Historical/Heritage based Sites</option>
-            <option value="option2">Religious Sites</option>
-            <option value="option3">Natural Sites</option>
+            <option value="Historical/Heritage">Historical/Heritage Sites</option>
+            <option value="Religious">Religious Sites</option>
+            <option value="Natural">Natural Sites</option>
           </select>
         </div>
 
         <div className="results">
-          <button>Show locations</button>
+          <button onClick={clickHandler}>Show tour locations</button>
+          <Location id = {category}/>
         </div>
-
       </div>
     </div>
   );
---
+++ src/components/Location.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import "./Location.css";
+import image1 from "../Images/Shaniwar_Wada_Attractions.jpg";
+import image2 from "../Images/Shreemant-Dagdusheth-Halwai-Sarvajanik-Ganpati-Pune.jpg";
+
+const Location = (props) => {
+    console.log(props) ;
+    const locationArray = [
+        {
+            image : image1 ,
+            name : "Shanivar-Wada" ,
+            time : "9:30 am - 5:30 pm" ,
+            category : "Historical/Heritage" ,
+        },
+        {
+            image : image2 ,
+            name : "Dagadusheth-Ganpati" ,
+            time : "6 am - 11 pm" ,
+            category : "Religious" ,
+        },
+    ]
+    return(
+        <div className="location">    
+         { 
+           props.id!=="" ? <> {
+           props.id==="Historical/Heritage" ? <>
+            <div className="card">
+                <div className="img">
+                    <img src={locationArray[0].image} alt="Image_of_location" />
+                </div>
+                <div className="info">
+                    <h3>Location Name : {locationArray[0].name}</h3>
+                    <h3>Timings to visit : {locationArray[0].time} </h3>
+                    <h3>Category of site : {locationArray[0].category} </h3>
+                    <button>Show location</button>
+                </div>
+            </div>
+            </>
+           :
+           <>  
+             <div className="card">
+                <div className="img">
+                    <img src={locationArray[1].image} alt="Image_of_location" />
+                </div>
+                <div className="info">
+                    <h3>Location Name : {locationArray[1].name}</h3>
+                    <h3>Timings to visit : {locationArray[1].time} </h3>
+                    <h3>Category of site : {locationArray[1].category} </h3>
+                    <button>Show location</button>
+                </div>
+            </div>
+          </>
+         }</> : <></>
+        }
+        </div>
+    )
+}
+
+export default Location ;
